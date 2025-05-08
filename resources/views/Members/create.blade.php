edaebae16075903bf19dbfe1f558651e9c8d941d
hashimAbbasJaffery <habbas2121@outlook.com>
2025-05-08 18:58:14 +0500

    changes in navbar

--- resources/views/Members/create.blade.php
+++ resources/views/Members/create.blade.php
@@ -111,7 +111,7 @@
                   <label class="block text-sm" style="margin-bottom: 20px;">
                     <span class="text-gray-700 dark:text-gray-400">Residential Address</span>
                     <input v-model="residential_address" type="text" style="width: 100% !important; margin-top: 4px !important;" data-message="residential_address_message" class="step_2 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Residential Address">
-                    <span style="display: none;" class="residential_address_message step_1_message text-xs text-red-600 dark:text-red-400">
+                    <span style="display: none;" class="residential_address_message step_2_message text-xs text-red-600 dark:text-red-400">
                       Residential Address field is required
                     </span>
                   </label>
@@ -122,6 +122,71 @@
                     <input v-model="city_country" type="text" style="width: 100% !important; margin-top: 4px !important;" data-message="city_country_message" class="step_2 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Residential Address">
                     <span style="display: none;" class="city_country_message step_1_message text-xs text-red-600 dark:text-red-400">
                       City/Country field is required
+                    </span>
+                  </label>
+                </div>
+              </div>
+              <div style="display: flex; justify-content: center;">
+                <div v-for="step in total_steps" class="step bg-purple-600" :style="{ 'background': step <= current_step ? '' : 'gray' }" style="width: 5px; height: 5px; border-radius: 100%; margin-right: 5px;">&nbsp;</div>
+              </div>
+            </div>
+            <div v-show="current_step === 3" class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+              <h5 style="margin-bottom: 20px;" class="dark:text-gray-200">Membership Details</h5>
+              <div style="display: flex; column-gap: 20px;">
+                <div style="width: 33.33%;">
+                  <label class="block text-sm" style="margin-bottom: 20px;">
+                    <span class="text-gray-700 dark:text-gray-400">Membership Type</span>
+                    <select v-model="membership_type" data-message="membership_type_field_message" class="step_3 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
+                      <option value="permanent">Permanent</option>
+                      <option value="permanent+">Permanent+</option>
+                      <option value="founder">Founder</option>
+                      <option value="corporate-applicant">Corporate Applicant</option>
+                      <option value="permanent se">Permanent SE</option>
+                      <option value="temporary">Temporary</option>
+                    </select>
+                    <span style="display: none;" class="membership_type_field_message step_3_message text-xs text-red-600 dark:text-red-400">
+                      Membership Type is required
+                    </span>
+                  </label>
+                </div>
+                <div style="width: 33.33%;">
+                  <label class="block text-sm" style="margin-bottom: 20px;">
+                    <span class="text-gray-700 dark:text-gray-400">Card Type</span>
+                    <select v-model="card_type" data-message="membership_type_field_message" class="step_3 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
+                      <option value="Provisional Membership">Provisional Membership</option>
+                      <option value="Cleared">Cleared</option>
+                    </select>
+                    <span style="display: none;" class="member_number_message step_1_message text-xs text-red-600 dark:text-red-400">
+                      Card Type field is required
+                    </span>
+                  </label>
+                </div>
+                <div style="width: 33.33%;">
+                  <label class="block text-sm" style="margin-bottom: 20px;">
+                    <span class="text-gray-700 dark:text-gray-400">File Number</span>
+                    <input v-model="file_number" type="text" style="width: 100% !important; margin-top: 4px !important;" data-message="email_message" class="step_3 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="File Number">
+                    <span style="display: none;" class="email_message step_1_message text-xs text-red-600 dark:text-red-400">
+                      File Number field is required
+                    </span>
+                  </label>
+                </div>
+              </div>
+              <div style="display: flex; column-gap: 20px;">
+                <div style="width: 50%;">
+                  <label class="block text-sm" style="margin-bottom: 20px;">
+                    <span class="text-gray-700 dark:text-gray-400">Date of Applying</span>
+                    <input v-model="date_of_applying" type="date" style="width: 100% !important; margin-top: 4px !important;" data-message="date_of_applying_message" class="step_3 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Residential Address">
+                    <span style="display: none;" class="date_of_applying_message step_1_message text-xs text-red-600 dark:text-red-400">
+                      Date of Applying field is required
+                    </span>
+                  </label>
+                </div>
+                <div style="width: 50%;">
+                  <label class="block text-sm" style="margin-bottom: 20px;">
+                    <span class="text-gray-700 dark:text-gray-400">Membership Number</span>
+                    <input v-model="membership_number" type="text" style="width: 100% !important; margin-top: 4px !important;" data-message="membership_number_message" class="step_3 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Membership Number">
+                    <span style="display: none;" class="membership_number_message step_1_message text-xs text-red-600 dark:text-red-400">
+                      Membership Number field is required
                     </span>
                   </label>
                 </div>
@@ -158,7 +223,13 @@
                 alt_phone_number: "",
                 email: "",
                 residential_address: "",
-                city_country: ""
+                city_country: "",
+
+                membership_type: "permanent",
+                membership_number: "",
+                card_type: "Provisional Membership",
+                file_number: "",
+                date_of_applying: ""
               }
             },
             methods: {
@@ -203,11 +274,11 @@
                 const phones = document.querySelectorAll(".phone")
                 
                 const updatePhoneNumber = () => {
-                  const countryData = iti.getSelectedCountryData();
-                  const country_code = document.getElementById("country_code");
-                  const phone_number = document.getElementById("phone_number");
-                  country_code.value = countryData.dialCode;
-                  phone_number.value = iti.getNumber().replace(/^\+/, '');
+                  // const countryData = iti.getSelectedCountryData();
+                  // const country_code = document.getElementById("country_code");
+                  // const phone_number = document.getElementById("phone_number");
+                  // country_code.value = countryData.dialCode;
+                  // phone_number.value = iti.getNumber().replace(/^\+/, '');
                 }
 
                 inputs.forEach(input => {
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,6 +14,7 @@ class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-
         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         @click="togglePagesMenu"
         aria-haspopup="true"
+        style="outline: none;"
       >
         <span class="inline-flex items-center">
           <svg class="w-5 h-5" style="width: 15px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M448 80l0 48c0 44.2-100.3 80-224 80S0 172.2 0 128L0 80C0 35.8 100.3 0 224 0S448 35.8 448 80zM393.2 214.7c20.8-7.4 39.9-16.9 54.8-28.6L448 288c0 44.2-100.3 80-224 80S0 332.2 0 288L0 186.1c14.9 11.8 34 21.2 54.8 28.6C99.7 230.7 159.5 240 224 240s124.3-9.3 169.2-25.3zM0 346.1c14.9 11.8 34 21.2 54.8 28.6C99.7 390.7 159.5 400 224 400s124.3-9.3 169.2-25.3c20.8-7.4 39.9-16.9 54.8-28.6l0 85.9c0 44.2-100.3 80-224 80S0 476.2 0 432l0-85.9z"/></svg>
@@ -72,6 +73,7 @@ class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-
         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         @click="toggleRecoveryMenu"
         aria-haspopup="true"
+        style="outline: none;"
       >
         <span class="inline-flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" style="width: 15px;" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M463.5 224l8.5 0c13.3 0 24-10.7 24-24l0-128c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8l119.5 0z"/></svg><span class="ml-4">Recovery</span>
@@ -136,6 +138,7 @@ class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-
         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         @click="toggleReciprocalMenu"
         aria-haspopup="true"
+        style="outline: none;"
       >
         <span class="inline-flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" style="width: 15px;" viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z"/></svg><span class="ml-4">Reciprocal</span>
@@ -200,6 +203,7 @@ class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-
         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         @click="toggleMembershipMenu"
         aria-haspopup="true"
+        style="outline: none;"
       >
         <span class="inline-flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" style="width: 15px;" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm80 256l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16L80 384c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
@@ -251,6 +255,7 @@ class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-
         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         @click="toggleComplainsMenu"
         aria-haspopup="true"
+        style="outline: none;"
       >
         <span class="inline-flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" style="width: 15px;" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>     <span class="ml-4">Complains &amp; Inquiry</span>
