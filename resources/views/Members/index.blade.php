2172616cc6bb97e250801733282808f7fb753d5e
hashimAbbasJaffery <habbas2121@outlook.com>
2025-05-29 14:47:09 +0500

    test

--- resources/views/Members/index.blade.php
+++ resources/views/Members/index.blade.php
@@ -55,7 +55,7 @@
               <div class="flex items-center text-sm">
                 <!-- Avatar with inset shadow -->
                 <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                  <img class="object-cover w-full h-full rounded-full" :src="`/storage/${member.profile_picture}`" alt="" loading="lazy">
+                  <img class="object-cover w-full h-full rounded-full" :src="`./storage/${member.profile_picture}`" alt="" loading="lazy">
                   <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                 </div>
                 <div>
