3b8cb247943d44abde46c810e74169072a80bcd7
hashimAbbasJaffery <habbas2121@outlook.com>
2025-05-29 18:11:14 +0500

    Bug Fix

--- public/assets/js/init-alpine.js
+++ public/assets/js/init-alpine.js
@@ -1,19 +1,11 @@
 function data() {
   function getThemeFromLocalStorage() {
-    // if user already changed the theme, use it
-    if (window.localStorage.getItem('dark')) {
-      return JSON.parse(window.localStorage.getItem('dark'))
-    }
 
-    // else return their preferences
-    return (
-      !!window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    )
+    return false;
   }
 
   function setThemeToLocalStorage(value) {
-    window.localStorage.setItem('dark', value)
+    window.localStorage.setItem('dark', false)
   }
 
   return {
