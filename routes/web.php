24f8b9c4771c3bb87a87f028099c5449dccb090f
hashimAbbasJaffery <habbas2121@outlook.com>
2025-05-29 14:40:01 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function() {
-    dd("Testing");
+    dd("Test");
 });
 
 Route::get('/', function () {
