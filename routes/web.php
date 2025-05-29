54e27656a1bbfb11a4b7655ef87572e78d6857e7
hashimAbbasJaffery <habbas2121@outlook.com>
2025-05-29 14:41:40 +0500

    Test

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function() {
+Route::get("/test", function() {
     dd("Test");
 });
 
