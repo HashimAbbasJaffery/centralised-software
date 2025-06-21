e076dcf5e135c45b1abd3ecc8fe71679e25f8f02
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-21 17:55:17 +0500

    Testing

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -24,7 +24,7 @@ class MemberController extends Controller
     public function index() {
         $this->user->isAllowedToPerformAction("member:manage");
 
-        $member = Member::filter()->orderBy("created_at", "desc")->paginate(4)->withQueryString();
+        $member = Member::filter()->orderBy("created_at", "desc")->paginate(8)->withQueryString();
         return MemberResource::collection($member);
     }
     public function store(MemberRequest $request) {
