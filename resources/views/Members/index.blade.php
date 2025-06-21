b84bc8bea3c2b25d1d0cf7b85c6aeda49c3f644d
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-21 17:21:34 +0500

    Layout Fix

--- resources/views/Members/index.blade.php
+++ resources/views/Members/index.blade.php
@@ -121,6 +121,7 @@
                     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                   </svg>
                 </button>
+                <br>
                 <button @click="getMember(member.id)" class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple">
                   <i class="fa-solid fa-eye" style="color: rgba(126, 58, 242, var(--text-opacity));"></i>
                 </button>
