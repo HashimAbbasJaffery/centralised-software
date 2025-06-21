3954776407f6e2708e76d3f05643d4a859cdc30a
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-21 15:00:53 +0500

    Debugging

--- app/Jobs/CreateFamilySheet.php
+++ app/Jobs/CreateFamilySheet.php
@@ -27,10 +27,10 @@ class CreateFamilySheet implements ShouldQueue
      */
     public function handle(): void
     {
-        Log::error($this->member);
         // $pdf = Pdf::loadView("Invoices.member_tree", [ "member" => $this->member ])
         // ->setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])
         // ->setPaper("A4", "portrait");
+        Log::info(Storage::disk('public')->exists($this->member->profile_picture));
         
         // $pdfContent = $pdf->output();
         // $fileName = $this->member->member_name . "-" . $this->member->id;
