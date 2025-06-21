db3990afd8f13916dd019802673fed16693b08d9
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-21 14:27:13 +0500

    Debugging

--- app/Jobs/CreateFamilySheet.php
+++ app/Jobs/CreateFamilySheet.php
@@ -31,10 +31,10 @@ class CreateFamilySheet implements ShouldQueue
         ->setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])
         ->setPaper("A4", "portrait");
         
-        $pdfContent = $pdf->output();
-        $fileName = $this->member->member_name . "-" . $this->member->id;
-        $filePath = "members/FamilySheet/" . $fileName . ".pdf";
-        Storage::disk("public")->put($filePath, $pdfContent);
+        // $pdfContent = $pdf->output();
+        // $fileName = $this->member->member_name . "-" . $this->member->id;
+        // $filePath = "members/FamilySheet/" . $fileName . ".pdf";
+        // Storage::disk("public")->put($filePath, $pdfContent);
 
         $this->member->has_receipt_created = true;
         $this->member->save();
