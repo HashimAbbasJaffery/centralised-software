1f2068c81d0490acac8b586c1304e4ae727d56f4
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-21 15:28:53 +0500

    Debugging

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get("testing-image", function() {
+    dd(storage_path("app/public"));
     $member = Member::latest()->first();
     $path = $member->profile_picture;
     $file = Storage::disk('public')->exists($path);
