d665e9da4761dbd643d8fc4b84d8384eb4db4614
hashimAbbasJaffery <habbas2121@outlook.com>
2025-06-30 16:58:38 +0500

    test

--- app/Preprocess/CleanMemberRequest.php
+++ app/Preprocess/CleanMemberRequest.php
@@ -18,11 +18,34 @@ class CleanMemberRequest
         $spouses = request()->spouses;
         $children = request()->children;
 
-        $children = collect($children)->filter(function($child) {
-            return !in_array(null, $child, true);
+        $children = collect($children)->filter(function ($child) {
+            $excludeKeys = ['profile_pic'];  // allow null in these keys
+
+            foreach ($child as $key => $value) {
+                if (in_array($key, $excludeKeys, true)) {
+                    continue;  // skip checking this field
+                }
+
+                if ($value === null) {
+                    return false;  // reject if any other field is null
+                }
+            }
+            return true;
         });
-        $spouses = collect($spouses)->filter(function($spouse) {
-            return !in_array(null, $spouse, true);
+
+        $spouses = collect($spouses)->filter(function ($spouse) {
+            $excludeKeys = ['picture'];  // allow null in these keys
+
+            foreach ($spouse as $key => $value) {
+                if (in_array($key, $excludeKeys, true)) {
+                    continue;
+                }
+
+                if ($value === null) {
+                    return false;
+                }
+            }
+            return true;
         });
 
         $phone_numbers = json_decode(request()->phone_numbers);
