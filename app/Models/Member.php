a2f3b444683c727a6579ef1388085169e59e5a0f
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-02 14:43:34 +0500

    Saved Event

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -77,7 +77,7 @@ class Member extends Model
             dispatch(new CreateFamilySheet($member));
         });
 
-        static::updated(function($member) {
+        static::saved(function($member) {
             dispatch(new SaveInGoogleDrive());
             dispatch(new CreateFamilySheet($member));
         });
