6212e14f0cb6c99a985512a3e92883ac5a9e421b
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-02 15:44:50 +0500

    Recent Member

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,13 @@ class HomeController extends Controller
 {
     public function index() {
         $members_monthly = Member::whereMonth('created_at', Carbon::now()->month)->count();
+        $recent_member = Member::latest()->limit(1)->get();
         $total_clubs = Club::count();
         $famous_club = Club::withCount("introletters")
                                 ->orderByDesc("introletters_count")
                                 ->limit(1)
                                 ->first();
         
-        return view("dashboard", compact("members_monthly", "total_clubs", "famous_club"));
+        return view("dashboard", compact("members_monthly", "total_clubs", "famous_club", "recent_member"));
     }
 }
