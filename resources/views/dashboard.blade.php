5981d9f8d35f47048b4c2d215eb4e64ee1b482eb
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-02 15:45:17 +0500

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,7 +62,7 @@
                     Recent Member
                   </p>
                   <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                    {{ $recent_member->member_name }}
+                    {{ $recent_member }}
                   </p>
                 </div>
               </div>
