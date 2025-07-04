331555a7948809425111603ae984037661eb0d46
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-04 14:08:45 +0500

    test

--- app/Jobs/SaveInGoogleDrive.php
+++ app/Jobs/SaveInGoogleDrive.php
@@ -149,7 +149,7 @@ class SaveInGoogleDrive implements ShouldQueue
         $permission = new Google_Service_Drive_Permission([
             'type' => 'user',
             'role' => 'writer', // use 'reader' if you only want to view
-            'emailAddress' => 'entirebackup.vg424@gmail.com' 
+            'emailAddress' => 'habbas21219@gmail.com' 
         ]);
         $driveService->permissions->create($spreadsheetId, $permission);
 
