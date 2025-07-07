92f42112f1f3e910c0b82f73453b4bce32f898ca
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-07 14:19:21 +0500

    Disable CSRF

--- routes/api.php
+++ routes/api.php
@@ -78,6 +78,17 @@ Route::patch("/spouse/{spouse}/patch", function(Spouse $spouse) {
     return request()->all();
 })->name("spouse.patch");
 Route::get("/member/all", [MemberController::class, "getAllMembers"])->name("api.member.all");
+Route::get("/member/fetch-by-membership", function() {
+    $membership_no = request()->membership_no;
+    $cnic_passport = request()->cnic_passport;
+
+    $member = Member::selectRaw("member_name AS full_name")
+                        ->where("membership_no", $membership_no)
+                        ->where("cnic_passport", $cnic_passport)
+                        ->first();
+    
+    return $member;
+})->withoutMiddleware([VerifyCsrfToken::class]);
 Route::get("/member/{member}/get", [MemberController::class, "getById"])->name("api.member.getById");
 Route::get("/members/all", [MemberController::class, "getAll"])->name("api.member.all");
 Route::get("/members", [MemberController::class, "index"])->name("api.member.index");
