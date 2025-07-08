87b91338158e4808334e045b7eb5e1fdc688f198
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-08 16:44:01 +0500

    test

--- resources/views/Members/member_detail.blade.php
+++ resources/views/Members/member_detail.blade.php
@@ -123,11 +123,22 @@
           </p>
         </div>
         <div v-show="tab === 'contact information'">
-          <p style="margin-top: 10px;"><span style="display: inline-block; width: 200px;">Phone Number:</span> {{ Str::replaceFirst("+", $member->phone_number_code, $member->phone_number) }}</p>
-          <p style="margin-top: 10px;"><span style="display: inline-block; width: 200px;">Alternate Phone Number:</span> {{ str_replace("+", $member->alternate_ph_number_code, $member->alternate_ph_number) }}</p>
+          <p style="margin-top: 10px;"><span style="display: inline-block; width: 200px;">Phone Number:</span> 
+            {{ Str::replaceFirst("+", $member->phone_number_code, Str::replaceFirst($member->phone_number_code, "", $member->phone_number)) }}
+          </p>
+          <p style="margin-top: 10px;"><span style="display: inline-block; width: 200px;">Alternate Phone Number:</span> 
+            {{ str_replace("+", $member->alternate_ph_number_code,  Str::replaceFirst($member->alternate_ph_number_code, "", $member->alternate_ph_number)) }}
+          </p>
           <p style="margin-top: 10px;"><span style="display: inline-block; width: 200px;">Emergency Number:</span>
             <span class="editable" data-editable="emergency_contact">
-              {{ Str::replaceFirst("+", $member->emergency_contact_code, Str::replaceFirst($member->emergency_contact_code, "", $member->emergency_contact)) }}
+              {{ Str::replaceFirst(
+                "+", 
+                $member->emergency_contact_code, 
+                Str::replaceFirst(
+                  $member->emergency_contact_code, 
+                  "", 
+                  $member->emergency_contact
+                )) }}
             </span>
             <input type="hidden" id="emergency_contact" value="{{ $member->emergency_contact }}"/>
           </p>
