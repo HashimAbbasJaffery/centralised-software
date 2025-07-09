10997c1c557c12d33991c6695d80f5f43d405eea
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-09 12:09:03 +0500

    delete spouse

--- resources/views/Members/member_detail.blade.php
+++ resources/views/Members/member_detail.blade.php
@@ -284,7 +284,7 @@
               <input type="hidden" id="spouse.blood_group.{{ $spouse->id }}" value="{{ $spouse->blood_group }}"/>
             <p>
           </div>
-          <button style="background: #e53935; color: white; width: 90%; margin: 0 auto; border-radius: 5px; margin-top: 10px;">Delete</button>
+          <button @click="divorce('{{ $spouse->id }}')" style="background: #e53935; color: white; width: 90%; margin: 0 auto; border-radius: 5px; margin-top: 10px;">Delete</button>
         </div>
       @endforeach
       <div @click="addSpouse" style="padding: 10px 10px; width: 33.33%; border: 1px dashed black; border-radius: 5px; margin-top: 30px; display: flex; align-items: center; justify-content: center; cursor: pointer;">
@@ -370,6 +370,9 @@
         }
       },
       methods: {
+        divorce(id) {
+          alert(id);
+        },
         changeSpousePicture(e) {
           const id = e.target.id.split(".")[1];
           const file = e.target.files[0];
