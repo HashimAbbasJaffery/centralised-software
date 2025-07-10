2fca89b723c4cb95e3e030f32a6200dd4f36d537
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-10 12:40:13 +0500

    Conditional Rendering

--- resources/views/Recovery/generate-report.blade.php
+++ resources/views/Recovery/generate-report.blade.php
@@ -312,7 +312,11 @@ footer{
                                 <td v-text="report.membership_number"></td>
                                 <td v-text="report.member_name"></td>
 
-                                <td v-text="report.phone_number?.replace('+', '').split(', ')[0] ?? '-'"></td>
+                                <td v-if="report.phone_number?.replace('+', '').split(', ')[0].length > 0" v-text="report.phone_number?.replace('+', '').split(', ')[0]"></td>
+                                <td v-else-if="report.phone_number?.replace('+', '').split(', ')[1].length > 0" v-text="report.phone_number?.replace('+', '').split(', ')[1]"></td>
+                                <td v-else-if="report.alternate_ph_number.length > 0" v-text="report.alternate_ph_number"></td>
+                                <td v-else>-</td>
+
                                 <td v-text="report.alternate_ph_number?.replace('+', '') ?? '-'">&nbsp;</td>
                                 
                                 <td class="text-right pe-2 latest_payable" v-text="report.recovery[0]?.payable.toLocaleString() ?? 'no updated'">no updated</td>
