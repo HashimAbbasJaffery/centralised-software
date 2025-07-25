edf2f28fe8787a6bc8cb0280c55ad5c746d20740
hashimAbbasJaffery <habbas2121@outlook.com>
2025-07-25 11:54:37 +0500

    Date

--- resources/views/Invoices/letter_invoice.blade.php
+++ resources/views/Invoices/letter_invoice.blade.php
@@ -162,15 +162,15 @@ h2{
             <td style="width: 20%;">To the reciprocal club</td>
             <td colspan="4">{{ $introletter->club->club_name }}</td>
             <td>Issue Date</td>
-            <td><?php echo date('d M Y', strtotime($introletter->created_at)); ?></td>
+            <td><?php echo date('d M Y', strtotime($introletter->issue_date)); ?></td>
         </tr> 
         <tr>
             <td>Member's name</td>
             <td colspan="2">{{ $introletter->member->member_name }}</td>
             <td>Membership No:</td>
             <td>{{ $introletter->member->membership_number }}</td>
-            <td>Expiry Date</td>
-            <td><?php echo date('d M Y', strtotime($introletter->created_at->addMonths($introletter->duration->months))); ?></td>
+                <td>Expiry Date</td>
+                <td><?php echo $introletter->issue_date->copy()->addMonths($introletter->duration->months)->format('d M Y'); ?></td>
         </tr> 
         
     </table>
