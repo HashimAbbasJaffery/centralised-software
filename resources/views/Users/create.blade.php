5c9c4f94d048b0a9f97dc8096976cd974481897c
HI-Developer25 <hamzarazzaque.work@gmail.com>
2025-11-18 11:38:37 +0500

    Updated placeholder of username field

--- resources/views/Users/create.blade.php
+++ resources/views/Users/create.blade.php
@@ -14,7 +14,7 @@
           class="step_1 block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700
                      focus:border-purple-400 focus:outline-none focus:shadow-outline-purple
                      dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-          placeholder="shahmir ahsanullah"
+          placeholder="Username"
           v-model="username"
         >
         <span class="member_field_message text-xs text-red-600 dark:text-red-400" style="display: none;">
